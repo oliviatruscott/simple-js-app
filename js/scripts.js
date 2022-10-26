3b5549450807c705d1e14a0f9504a0f87e01d9a9
oliviatruscott <103083352+oliviatruscott@users.noreply.github.com>
2022-10-26 11:42:58 -0700

    replaced test code with assignment 1.1 code

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,11 @@
 // use camelCase for variables
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
+
+alert('Hello world');
+
+let favoriteFood = 'Pho';
+document.write(favoriteFood);
 
 //used for smaller notes, everything after the slashes is ignored
 
 /* used for longer notes 
 spaning multiple lines */
-
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
