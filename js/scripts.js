fd07cd608019b225e3b5aeff59d7f6efab98a51a
oliviatruscott <103083352+oliviatruscott@users.noreply.github.com>
2022-10-26 11:36:53 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// use camelCase for variables
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+//used for smaller notes, everything after the slashes is ignored
+
+/* used for longer notes 
+spaning multiple lines */
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
