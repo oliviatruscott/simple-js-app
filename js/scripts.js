c389cfa414b2b4c91d2be4089c55cc34f0f96609
oliviatruscott <103083352+oliviatruscott@users.noreply.github.com>
2022-10-27 14:24:12 -0700

    added pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
 // use camelCase for variables
-
-alert('Hello world');
-
-let favoriteFood = 'Pho';
-document.write(favoriteFood);
-
 //used for smaller notes, everything after the slashes is ignored
-
 /* used for longer notes 
 spaning multiple lines */
+
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        entrynumber: 1,
+        height: 0.7,
+        types: ['Grass','Poison'],
+    },
+    {
+        name: 'Ivysaur',
+        entrynumber: 2,
+        height: 1,
+        types: ['Grass','Poison'],
+    },
+    {
+        name: 'Venusaur',
+        entrynumber: 3,
+        height: 2,
+        types: ['Grass','Poison'],
+    }
+
+]
