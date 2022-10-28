332b9e1fa8fe60b7c0ec18c2539dc45652a45aa4
oliviatruscott <103083352+oliviatruscott@users.noreply.github.com>
2022-10-28 16:15:15 -0700

    created loop for display and height statement

--- js/scripts.js
+++ js/scripts.js
@@ -6,21 +6,40 @@ spaning multiple lines */
 let pokemonList = [
     {
         name: 'Bulbasaur',
-        entrynumber: 1,
+        entryNumber: 1,
         height: 0.7,
         types: ['Grass','Poison'],
     },
     {
         name: 'Ivysaur',
-        entrynumber: 2,
+        entryNumber: 2,
         height: 1,
         types: ['Grass','Poison'],
     },
     {
         name: 'Venusaur',
-        entrynumber: 3,
+        entryNumber: 3,
         height: 2,
         types: ['Grass','Poison'],
+    },
+    {
+        name: 'Onix',
+        entryNumber: 95,
+        height: 8.8,
+        types: ['Rock','Ground'],
     }
+];
 
-]
+//loop prints list of pokemon, adds statement based on height
+for (let i = 0; i < pokemonList.length; i++){
+    if(pokemonList[i].height >= 5){
+        document.write("#" + pokemonList[i].entryNumber + " " + pokemonList[i].name + " (height: " + pokemonList[i].height + "m) Wow, thats big!<br>")
+    }
+    else if(pokemonList[i].height <= 1){
+        document.write("#" + pokemonList[i].entryNumber + " " + pokemonList[i].name + " (height: " + pokemonList[i].height + "m) Wow, thats small!<br>")
+    }
+    else {
+        //would this also be acceptible code?
+        document.write(`#${pokemonList[i].entryNumber} ${pokemonList[i].name} (height: ${pokemonList[i].height}m)<br>`)
+    }
+}
