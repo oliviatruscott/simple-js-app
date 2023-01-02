0948a9bc68926f340e251d65bbde9bd60b28703a
oliviatruscott <103083352+oliviatruscott@users.noreply.github.com>
2023-01-02 14:19:14 -0800

    added addListItem to IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -13,37 +13,51 @@ let pokemonRepository = (function() {
     function getAll () {
         return pokemonList
     };
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    };
+    //button list of pokemon
+    function addListItem(pokemon){
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listPokemon = document.createElement('li');
+        let button = document.createElement('button');
+            button.innerText = pokemon.name;
+            button.classList.add('pokemon-button');
+        listPokemon.appendChild(button);
+        pokemonList.appendChild(listPokemon);         //error listed here???
+        //event listener, console logs when button is clicked
+        button.addEventListener('click', function() {
+            showDetails(pokemon);
+    });
+    };
+
     return {
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addListItem: addListItem
     };
 }) ();
 
 //using add function
 pokemonRepository.add({ name: 'Onix', entryNumber: 95, height: 8.8, types: ['Rock','Ground'] });
 
-//forEach loop, prints pokemon info with extra comment on size
-//pokemonList is now a local variable and must be 'called' upon by using getAll function inside pokemonRepository
+console.log(pokemonRepository.getAll());
+
+//for each loop goes through addListitem function
 pokemonRepository.getAll().forEach(function(pokemon) {
-    if(pokemon.height >= 5){
-        document.write(`#${pokemon.entryNumber} ${pokemon.name} (height: ${pokemon.height}m) Wow, thats big!<br>`)
-    }
-    else if(pokemon.height <= 1){
-        document.write(`#${pokemon.entryNumber} ${pokemon.name} (height: ${pokemon.height}m) Wow, thats small!<br>`)
-    }
-    else {
-        document.write(`#${pokemon.entryNumber} ${pokemon.name} (height: ${pokemon.height}m)<br>`)
-    }
+    pokemonRepository.addListItem(pokemon);
 });
 
+
+
 //filter pokemon by name
-let pokemonNames = []; //empty array for names
-for(let i = 0; i < pokemonRepository.getAll().length; i++) { //adds names into array
-    pokemonNames.push(pokemonRepository.getAll()[i].name);
-};
+//let pokemonNames = []; //empty array for names
+//for(let i = 0; i < pokemonRepository.getAll().length; i++) { //adds names into array
+//    pokemonNames.push(pokemonRepository.getAll()[i].name);
+//};
 
-function findPokemon (query) { //function for searching pokemon
-    return pokemonNames.filter(name => name.toLowerCase().indexOf(query.toLowerCase()) > -1);
-};
+//function findPokemon (query) { //function for searching pokemon
+//    return pokemonNames.filter(name => name.toLowerCase().indexOf(query.toLowerCase()) > -1);
+//};
 
-console.log(findPokemon('On')); //calls upon search function with partial pokemon name
+//console.log(findPokemon('On')); //calls upon search function with partial pokemon name
